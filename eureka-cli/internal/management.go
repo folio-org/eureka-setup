b270934b1721f8e0bf0dc41519b1f40c0a6f0af5
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2024-11-22 00:34:25 +0500

    Add disable interface validation to mgr-tenant-entitlements, set loadSample=true during entitlement

--- eureka-cli/internal/management.go
+++ eureka-cli/internal/management.go
@@ -355,7 +355,7 @@ func RemoveTenantEntitlements(commandName string, enableDebug bool, panicOnError
 
 // TODO Add depCheck=false flag
 func CreateTenantEntitlement(commandName string, enableDebug bool) {
-	requestUrl := fmt.Sprintf(HostDockerInternalUrl, TenantEntitlementsPort, "/entitlements?purgeOnRollback=true&ignoreErrors=false&tenantParameters=loadReference=false,loadSample=false")
+	requestUrl := fmt.Sprintf(HostDockerInternalUrl, TenantEntitlementsPort, "/entitlements?purgeOnRollback=true&ignoreErrors=false&tenantParameters=loadReference=false,loadSample=true")
 	applicationMap := viper.GetStringMap(ApplicationKey)
 	applicationName := applicationMap["name"].(string)
 	applicationVersion := applicationMap["version"].(string)
