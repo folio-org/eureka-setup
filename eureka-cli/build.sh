f893abfb16b56033992ac854a4473e807fb9feca
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2026-01-14 17:46:52 +0500

    Add version tracking

---
+++ eureka-cli/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export GOOS=windows
+export GOARCH=amd64
+
+VERSION=$(git describe --tags --always --dirty)
+COMMIT=$(git rev-parse --short HEAD)
+BUILD_DATE=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+PACKAGE="github.com/folio-org/eureka-cli/cmd"
+BIN="./bin/eureka-cli.exe"
+
+echo "> Building binary"
+go build -ldflags \
+  "-X '$PACKAGE.Version=$VERSION' -X '$PACKAGE.Commit=$COMMIT' -X '$PACKAGE.BuildDate=$BUILD_DATE'" \
+  -o $BIN .
+echo "> Binary built completed"
+
+echo "> Installing binary"
+go install
+echo "> Binary install completed"
